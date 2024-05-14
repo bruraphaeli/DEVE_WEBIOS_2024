5bbfc4a78a0d3a508b885e91f65102050af8c7e5
bruraphaeli <taty.bruno.bruna@gmail.com>
2024-05-13 21:45:16 -0300

    JS-02-03

---
+++ JS/JS03-String/main.js
@@ -0,0 +1,66 @@
+var string = "Os alunos estão";
+var string2 = "Reprovados";
+var string3 = "Ou aprovados";
+var num1 = 2
+var num2 = 4
+var olhaai = "Os repeovados"
+
+
+
+console.log(string + string2 + string3);
+console.log("Os alunos estão" + "Reprovados" + "Ou aprovados")
+
+
+
+
+console.log(`${string + string2 + string3 + (num1 * num2) + olhaai}`)
+
+
+
+console.clear()
+
+
+var texto1 = "IOS fez uma excursão na TOTVS com três alunos"
+
+console.log(texto1.charAt(27));
+console.log(texto1[27]);
+
+
+console.clear
+
+
+var texto2 = "IOS"
+var texto3 = "Está chovendo hamburguer"
+
+console.log (`na variavel texto2 tem ${texto2.length}caracteres`)
+
+console.log(`na variavel text3 tem ${texto3.length} caracteres`)
+
+var nome = "joão"
+console.log (nome.length)
+
+
+var texto4 = "aqui está em minusculo"
+var texto5 = "AQUI ESTÁ EM MAIUSCULO"
+
+console.log(`Minusculo: ${ texto5.toUpperCase()}`)
+console.log(`Maiusculo: ${ texto4.toUpperCase()}`)
+
+
+var str= "Mozilla"
+
+console.log(str)
+console.log(str.substring(1,3))
+console.log(str.substring(3))
+
+console.clear()
+
+var texto6= "Olha a pipoca"
+console.log(texto6)
+console.log(texto6.replace(`pipoca`,`Carreta Furacão`) )
+
+
+console.clear()
+
+var texto7= "      Olha Carreta       "
+console.log(texto7.trim())
