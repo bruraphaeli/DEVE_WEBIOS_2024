079fd23684bdbfe92dadc1da1b88456504426378
bruraphaeli <taty.bruno.bruna@gmail.com>
2024-05-16 19:38:12 -0300

    JS03-String JS04-Condicional

--- JS/JS05-Repeticao/ExercicioCasa/main.js
+++
@@ -1,6 +0,0 @@
-var texto = "Estou aprendendo JavaScript e serei uma futura DEVS"
-
-console.log(texto.charAt(19))
-console.log(`na variavel texto tem ${texto.length} caracteres`)
-console.log(`${texto.toUpperCase()}`)
-console.log(texto.substring(48,52))
