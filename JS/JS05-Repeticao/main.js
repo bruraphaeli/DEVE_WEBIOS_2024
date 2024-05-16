09ad814e1735a040ffc2a84a86b5ad471079ebd6
bruraphaeli <taty.bruno.bruna@gmail.com>
2024-05-15 21:40:16 -0300

    JS05-Repetição

---
+++ JS/JS05-Repeticao/main.js
@@ -0,0 +1,11 @@
+// WHILE
+// Enquando 0 < 8 vou apresentar a mensagem 0 ainda é menor que 10
+
+var contador = 0
+
+
+do{
+   console.log(contador + "ainda é menor que 10")
+   contador ++;
+
+}while(contador < 10)
