1b4fc2f29f397818f8385316003237fed841667a
bruraphaeli <taty.bruno.bruna@gmail.com>
2024-05-17 20:14:13 -0300

    JS05-Repetição

---
+++ JS/JS05-Repeticao/ExercicioCasa/main.js
@@ -0,0 +1,9 @@
+var tabuada = 2
+
+if(tabuada ===2 || tabuada===3){
+    console.log(`tabuada do numero ${tabuada}:`)
+
+    for (var numero = 1; numero<=10; numero++){
+        console.log(`${tabuada} x ${numero} = ${tabuada * numero}`)
+    }
+}
