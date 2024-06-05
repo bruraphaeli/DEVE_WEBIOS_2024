7619a537cbb0dcb43b67c481c10d753feebf4969
bruraphaeli <taty.bruno.bruna@gmail.com>
2024-06-04 21:25:52 -0300

    JS11-DOM-Parte1

---
+++ JS/JS11-DOM-Parte1/main.js
@@ -0,0 +1,26 @@
+var time = 1
+if ( time == 1){
+    document.body.style.background = "red"
+}else if ( time == 0){
+    document.body.style.background = "Black"
+}else{
+    document.body.style.background = "green"
+}
+
+
+
+window.prompt ("Deseja realmente sair desse jogo? ")
+
+var time = prompt("Qual o nome do time?")
+if(time == "Sao Paulo"){
+    alert("O time é Sao Paulo")
+    document.body.style.backgroundColor = "red"
+}else if(time == "Corinthians"){
+    alert("O time é o Corinthians")
+    document.body.style.background = "Black"
+}else if{
+    alert( "O time é Palmeiras")
+}else{
+    alert("Não sei qual é o time")
+}
+
